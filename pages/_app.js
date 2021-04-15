de86b38007de55f9471911f6e930a5a292f149fd
Claudio Alcantara <claudiomar.alcantara@gmail.com>
2021-04-15 17:31:58 -0300

    feat: trigger web vitals report

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,19 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+export function reportWebVitals({ id, name, label, value }) {
+  // Analytics 4 compability
+  gtag("event", name, {
+    // Built-in params:
+    value: delta, // Use `delta` so the value can be summed.
+    // Custom params:
+    metric_id: id, // Needed to aggregate events.
+    metric_value: value, // Optional.
+    metric_delta: delta, // Optional.
+  });
 }
 
-export default MyApp
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />;
+}
+
+export default MyApp;
